5a3cbf2e2744348d36f5e4edba2e317c06d7f001
Jose Garcia <jagarcia7655@gmail.com>
2020-10-15 20:34:53 -0500

    Optimizando al máximo nuestra app y configurando el servidor para produccion

--- src/server/server.js
+++ src/server/server.js
@@ -4,6 +4,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import webpack from 'webpack';
 import React from 'react';
+import helmet from 'helmet';
 import { renderToString } from 'react-dom/server';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
@@ -12,6 +13,7 @@ import { StaticRouter } from 'react-router-dom';
 import serverRoutes from '../frontend/routes/serverRoutes';
 import reducer from '../frontend/reducers';
 import initialState from '../frontend/initialState';
+import getManifest from './getManifest';
 
 dotenv.config();
 
@@ -28,15 +30,27 @@ if (ENV === 'development') {
 
   app.use(webpackDevMiddleware(compiler, serverConfig));
   app.use(webpackHotMiddleware(compiler));
+} else {
+  app.use((req, res, next) => {
+    if (!req.hashManifest) req.hashManifest = getManifest();
+    next();
+  });
+  app.use(express.static(`${__dirname}/public`));
+  app.use(helmet());
+  app.use(helmet.permittedCrossDomainPolicies());
+  app.disable('x-powered-by');
 }
 
-const setResponse = (html, preloadedState) => {
+const setResponse = (html, preloadedState, manifest) => {
+  const mainStyles = manifest ? manifest['main.css'] : 'assets/app.css';
+  const mainBuild = manifest ? manifest['main.js'] : 'assets/app.js';
+
   return (`
     <!DOCTYPE html>
   <html lang="en">
     <head>
       <meta charset="UTF-8" />
-      <link rel="stylesheet" href="assets/app.css" type="text/css" />
+      <link rel="stylesheet" href=${mainStyles} type="text/css" />
       <title>Platzi Video</title>
     </head>
     <body>
@@ -44,7 +58,7 @@ const setResponse = (html, preloadedState) => {
       <script>
       window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(/</g, '\\u003c')}
       </script>
-      <script src="assets/app.js" type="text/javascript" ></script>
+      <script src=${mainBuild} type="text/javascript" ></script>
     </body>
   </html>
   `);
@@ -61,12 +75,12 @@ const renderApp = (req, res) => {
     </Provider>,
   );
 
-  res.send(setResponse(html, preloadedState));
+  res.send(setResponse(html, preloadedState, req.hashManifest));
 };
 
 app.get('*', renderApp);
 
 app.listen(PORT, (err) => {
   if (err) log.error(err);
-  else console.log('App corriendo en localhost:3000');
+  else console.log(`App corriendo en http://localhost:${PORT}`);
 });
