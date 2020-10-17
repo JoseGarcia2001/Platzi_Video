c3dbfd06bd3592768ceaf9486e4175dd2ab4cc7e
Jose Garcia <jagarcia7655@gmail.com>
2020-10-17 13:24:11 -0500

    Finalización del curso

---
+++ nextjs/pages/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const page = () => <div> Hello World next JS :D </div>;
+
+export default page;
--- src/server/getManifest.js
+++ src/server/getManifest.js
@@ -1,5 +1,6 @@
 import fs from 'fs';
 
+// eslint-disable-next-line consistent-return
 const getManifest = () => {
   try {
     return JSON.parse(fs.readFileSync(`${__dirname}/public/manifest.json`));
