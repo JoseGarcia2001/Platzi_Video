ad8a7b58a53dc40befbac9c47284c8d8b382e273
Jose Garcia <jagarcia7655@gmail.com>
2020-11-17 22:39:41 -0500

    precommit

--- src/frontend/actions/index.js
+++ src/frontend/actions/index.js
@@ -1,4 +1,5 @@
-// eslint-disable-next-line import/prefer-default-export
+import axios from 'axios';
+
 export const setFavorite = (payload) => ({
   type: 'SET_FAVORITE',
   payload,
@@ -33,3 +34,43 @@ export const searchingVideo = (payload) => ({
   type: 'SEARCHING_VIDEO',
   payload,
 });
+
+export const setError = (payload) => ({
+  type: 'SET_ERROR',
+  payload,
+});
+
+export const registerUser = (payload, redirectURL) => {
+  return (dispatch) => {
+    axios
+      .post('/auth/sign-up', payload)
+      .then(({ data }) => dispatch(registerRequest(data)))
+      .then(() => {
+        window.location.href = redirectURL;
+      })
+      .catch((error) => dispatch(setError(error)));
+  };
+};
+
+export const loginUser = (payload, redirectURL) => {
+  return (dispatch) => {
+    axios({
+      url: '/auth/sign-in',
+      method: 'post',
+      auth: {
+        username: payload.email,
+        password: payload.password,
+      },
+    })
+      .then(({ data }) => {
+        document.cookie = `email=${data.user.email}`;
+        document.cookie = `name=${data.user.name}`;
+        document.cookie = `id=${data.user.id}`;
+        dispatch(loginRequest(data.user));
+      })
+      .then(() => {
+        window.location.href = redirectURL;
+      })
+      .catch((err) => setError(err));
+  };
+};
